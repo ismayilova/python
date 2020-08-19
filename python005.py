5c6b5b17b571d4db7ce6ae6fa8293014f0e0477d
repl.it user <replituser@example.com>
2020-08-19 06:06:26 +0000

    Day 5

---
+++ python005.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(1,11):
+  #print(str(n)+" x "+str(i)+" = "+str(n*i))
+  # print('%s x %s = %s'%(n,i,(n*i)))
+  print(f'{n} x {i} = {n*i}')
