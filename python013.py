d5641ae2094864d3e4affe6915bdc20d67377626
repl.it user <replituser@example.com>
2021-03-01 09:27:47 +0000

    Day 13 abstract class

---
+++ python013.py
@@ -0,0 +1,31 @@
+# import Lesson2.py
+# import Lesson1.py
+
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+
+
+class MyBook(Book):
+  def __init__(self,title,author,price):
+    self.title  = title
+    self.author = author
+    self.price = price
+
+  
+  def  display(self):
+    print(f'Title: {self.title}')
+    print(f'Author: {self.author}')
+    print(f'Price: {self.price}')
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+print(new_novel.price)
+new_novel.display()
